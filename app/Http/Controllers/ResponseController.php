b40816f1a69338c46f9a24dd77e2e9460827eb80
ArsalanMoravvej <moravvejmohammad97@gmail.com>
2025-05-19 15:05:40 +0330

    Add Response model, controller, requests, policy, factory, migration, and seeder with basic structure

---
+++ app/Http/Controllers/ResponseController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Response;
+use App\Http\Requests\StoreResponseRequest;
+use App\Http\Requests\UpdateResponseRequest;
+
+class ResponseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreResponseRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Response $response)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateResponseRequest $request, Response $response)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Response $response)
+    {
+        //
+    }
+}

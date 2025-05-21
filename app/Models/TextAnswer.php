fe3fbb44190d67efb3f3fb12ca14ea28f33912b6
ArsalanMoravvej <moravvejmohammad97@gmail.com>
2025-05-21 06:15:29 +0330

    Add TextAnswer model, factory, migration, and seeder

---
+++ app/Models/TextAnswer.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class TextAnswer extends Model
+{
+    /** @use HasFactory<\Database\Factories\TextAnswerFactory> */
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function question(): BelongsTo
+    {
+        return $this->belongsTo(Question::class);
+    }
+}
